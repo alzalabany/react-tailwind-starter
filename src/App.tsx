6acab5bee2834fb215f151afab79bf7201300b51
Momen <alzalabany@gmail.com>
2021-10-20 05:48:00 +0200

    add eslint and basic react libs

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ function App() {
       <div className="w-3/4 my-auto ml-20">
         <h1 className="text-5xl bg-blue-600 font-bold mb-10 text-white">{'React <3 Tailwind'}</h1>
         <p className="text-white">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo officia earum ducimus neque obcaecati consequuntur ratione accusamus at officiis tempore,
-          magnam non debitis fugit unde alias id quidem necessitatibus.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo officia earum ducimus neque obcaecati consequuntur ratione accusamus at
+          officiis tempore, magnam non debitis fugit unde alias id quidem necessitatibus.
         </p>
-      </div> 
+      </div>
     </div>
   );
 }
